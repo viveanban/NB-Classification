f3188b45a9a05bd6a4977d1756c4baa5aaaba34d
viveka <viveka98@hotmail.ca>
2020-04-05 19:45:40 -0400

    final version with ordered languages

--- Main.py
+++ Main.py
@@ -7,16 +7,16 @@ import sys
 
 def main():
     # # Read from standard input
-    # vocab = int(sys.argv[1])
-    # n = int(sys.argv[2])
-    # smoothing_value = float(sys.argv[3])
-    # training_file = sys.argv[4]
-    # test_file = sys.argv[5]
+    vocab = int(sys.argv[1])
+    n = int(sys.argv[2])
+    smoothing_value = float(sys.argv[3])
+    training_file = sys.argv[4]
+    test_file = sys.argv[5]
 
-    # nb = Classifier(vocab, n, smoothing_value)
-    nb = CustomizedModel(3, 3, 0.05)
-    nb.train("training.txt")
-    nb.test("test.txt")
+    nb = Classifier(vocab, n, smoothing_value)
+    # nb = CustomizedModel(vocab, n, smoothing_value)
+    nb.train(training_file)
+    nb.test(test_file)
     Evaluation(nb)
     ErrorAnalysis(nb)
 
