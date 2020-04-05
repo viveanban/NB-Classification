a0a17f56bf1cdeeaa9081e9a18de576840b9d628
Tarek Ait Hamouda <tarekaithamouda@gmail.com>
2020-04-05 18:38:01 -0400

    changed customized model

--- CustomizedModel.py
+++ CustomizedModel.py
@@ -1,43 +1,29 @@
 from GaussianNaiveBayesClassifier import Classifier
-
+from nltk.corpus import stopwords
 
 class CustomizedModel(Classifier):
 
     def __init__(self, vocab, nGram_size, smoothing_value):
         super().__init__(vocab, nGram_size, smoothing_value)
-        self.cluesDb = dict()
-        self.cluesDb["es"] = {"é", "á", "ó", "ú", "ü", "í", "ü", "ñ"}
-        self.cluesDb["fr"] = {"ç", "à", "è", "î", "û", "ô", "ö", "ï", "ü", "ù", "`"}
-        self.cluesDb["ca"] = {"ç", "á", "é", "í", "ó", "ú", "à", "è", "î", "û", "ô", "ö", "ï", "ü", "ù", "`", "õ", "ñ"}
+
 
     def calculate_nb_score(self, tweet_list: list, lang: str):
 
         prior_prob = self.prior_probability(lang)
         ngram_list = self.get_ngrams_given_word_list(tweet_list)
+        self.languageMap  = {"en":"english", "es":"spanish", "ca":"catalan", "gl":"galician", "pt":"portuguese", "eu":"basque"}
 
         conditional_prob = 0
         for ngram in ngram_list:
             conditional_prob += self.conditional_probability(ngram, lang)
 
-        return self.get_frequency_of_clues(lang, tweet_list) * 0.2 * (prior_prob + conditional_prob)
+        return self.get_frequency_of_clues(lang, tweet_list) * (prior_prob + conditional_prob)
 
     def get_frequency_of_clues(self, lang, tweet_list):
-        count = self.get_sentence_character_frequency(tweet_list)
-        frequency_of_clues = 0
 
-        for lang in self.cluesDb:
-            for clue in self.cluesDb[lang]:
-                frequency_of_clues += count.get(clue, 0)
-
-        return frequency_of_clues
-
-    def get_sentence_character_frequency(self, tweet_list: list):
-        count ={}
-        for tweet_word in tweet_list:
-            list_of_characters = tweet_word.split()
-            for character in list_of_characters:
-                if character in count:  # check if it exists in dictionary
-                    count[tweet_word] += 1
-                else:
-                    count[tweet_word] = 1  # first occurrence of character
-        return count
+        score = 0
+        stopWords = set(stopwords.words(self.languageMap[lang]))
+        for word in tweet_list:
+            if word in stopWords:
+                score += 1
+        return score
--- Main.py
+++ Main.py
@@ -6,19 +6,18 @@ import sys
 
 
 def main():
-    # Read from standard input
-    vocab = int(sys.argv[1])
-    n = int(sys.argv[2])
-    smoothing_value = float(sys.argv[3])
-    training_file = sys.argv[4]
-    test_file = sys.argv[5]
+    # # Read from standard input
+    # vocab = int(sys.argv[1])
+    # n = int(sys.argv[2])
+    # smoothing_value = float(sys.argv[3])
+    # training_file = sys.argv[4]
+    # test_file = sys.argv[5]
 
-    nb = Classifier(vocab, n, smoothing_value)
-    # nb = CustomizedModel(vocab, n, smoothing_value)
-    nb.train(training_file)
-    nb.test(test_file)
+    # nb = Classifier(vocab, n, smoothing_value)
+    nb = CustomizedModel(3, 3, 0.05)
+    nb.train("training.txt")
+    nb.test("test.txt")
     Evaluation(nb)
     ErrorAnalysis(nb)
-
 
 main()
