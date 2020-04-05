a70a2b657d0e6d7d6f0e8a4fe1f63a236f0c8087
viveka <viveka98@hotmail.ca>
2020-04-05 09:08:54 -0400

    Added standard input
    Remember to pass the params in the correct order (5 params -> vocab, n, smoothing value, training file, test file)

--- Main.py
+++ Main.py
@@ -1,12 +1,22 @@
 from GaussianNaiveBayesClassifier import Classifier
 from Evaluation import Evaluation
 from ErrorAnalysis import ErrorAnalysis
+from CustomizedModel import CustomizedModel
+import sys
 
 
 def main():
-    nb = Classifier(1, 2, 1)
-    nb.train("training.txt")
-    nb.test("test.txt")
+    # Read from standard input
+    vocab = int(sys.argv[1])
+    n = int(sys.argv[2])
+    smoothing_value = float(sys.argv[3])
+    training_file = sys.argv[4]
+    test_file = sys.argv[5]
+
+    # nb = Classifier(vocab, n, smoothing_value)
+    nb = CustomizedModel(vocab, n, smoothing_value)
+    nb.train(training_file)
+    nb.test(test_file)
     Evaluation(nb)
     ErrorAnalysis(nb)
 
